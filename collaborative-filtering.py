6bf7fc23037b516d3aaf2983bd5df31ac73f7298
vvusirikala <vvusirikala@rmn.com>
2017-02-22 18:23:29 -0600

    RMSE and dat files

--- collaborative-filtering.py
+++ collaborative-filtering.py
@@ -26,6 +26,7 @@ def read_csv_data(filename):
 
     return (user_ind, movie_ind, ratings)
 
+
 def read_dat_data(filename):
     ratings_df = pd.read_csv(filename, sep='::', header=None)
     ratings_df.columns = ["userId", "movieId", "rating", "timestamp"]
@@ -43,19 +44,17 @@ def read_dat_data(filename):
 
     return (user_ind, movie_ind, ratings)
 
-def convert_to_um_matrix(users, movies, ratings):
-#    print(str(len(set(users))))
-#    print(str(len(set(movies))))
 
+def convert_to_um_matrix(users, movies, ratings):
     um = csr_matrix((ratings, (users, movies)), shape=(len(user_id_map), len(movie_id_map)))
     return um
 
+
 def compute_user_similarity(um):
     s_user = cosine_similarity(um, um, dense_output=False)
-#    assert all(s_user == s_user.T)
-    #print(s_user[:5][:5])
     s_user.setdiag(0)
     return s_user
+
 
 def compute_movie_similarity(um):
     s_movie = cosine_similarity(um.T, um.T, dense_output=False)
@@ -92,22 +91,28 @@ def fold(users, movies, ratings):
             if mov_rating:
                 user_actual_list.append((user, mov_rating))
 
-        print(precision_at_N(um_dense, user_actual_list))
+        print(rmse(um_dense, user_actual_list))
+        # print(precision_at_N(um_dense, user_actual_list))
 
 
 def precision_at_N(um_dense, user_actual_list, top_N=6):
     precisions = []
     for user, movie_ratings in user_actual_list:
         top_sorted_actual = sorted(movie_ratings, key=lambda x : x[1])[::-1][:top_N]
-#        print(um_dense[user].shape)
         top_sorted_predicted = np.argsort(um_dense[user]).tolist()[::-1][:top_N]
-#        print(len(top_sorted_predicted[0]))
-#        print(top_sorted_actual[:5])
-#        print(top_sorted_predicted[:5])
         overlap = len({m for m,r in top_sorted_actual} & set(top_sorted_predicted))
         total_rated = min(len(top_sorted_actual), len(top_sorted_predicted))
         precisions.append(overlap / total_rated)
     return np.mean(precisions)
+
+
+def rmse(um_dense, user_actual_list):
+    errors = []
+    for user, movie_ratings in user_actual_list:
+        for movie, actual_rating in movie_ratings:
+            predicted_rating = um_dense[user][movie]
+            errors.append((predicted_rating - actual_rating)**2)
+    return np.sqrt(np.mean(errors))
 
 
 def fill_hash_matrix(um, dist, ind, n_neighbors=6):
@@ -120,17 +125,11 @@ def fill_matrix(s_user, um):
     um_dense = np.dot(s_user, um)
     s_sum = s_user.sum(axis=0)
     return (um_dense.T / s_sum).T
-#    return um_dense
-#    return np.divide(um_dense, s_sum)
 
 
 if __name__ == "__main__":
     filename = sys.argv[1]
-    users, movies, ratings = read_csv_data(filename) if filename[-3] == 'csv' else read_dat_data(filename)
+    users, movies, ratings = read_csv_data(filename) if filename[-3:] == 'csv' else read_dat_data(filename)
     fold(users, movies, ratings)
-#    um = convert_to_um_matrix(users, movies, ratings)
-#    s_user = compute_user_similarity(um)
-#    um_dense = fill_matrix(s_user, um)
-#    print(um_dense[:5][:5])
 
     print("Done.")
