62f22f13b75cd883623271dad3fe2b8f833c3de5
Caelan Evans <evans.caelan@gmail.com>
2017-04-21 17:33:21 -0500

    Fixed download

--- download.py
+++ download.py
@@ -1,13 +1,29 @@
 import sys
+import io
+import requests
 import pandas as pd
+import zipfile
+import StringIO
+import os
+import shutil
 
-urls = {"ml-small": "http://files.grouplens.org/datasets/movielens/ml-latest-small.zip"}
+url = "http://files.grouplens.org/datasets/movielens/ml-latest-small.zip"
 
-df = pd.read_csv(urls[sys.argv[1]], compression='zip')
+r = requests.get(url)
+
+z = zipfile.ZipFile(StringIO.StringIO(r.content))
+
+df = pd.read_csv(z.open("ml-latest-small/ratings.csv"), sep=',')
+
 user_id_map = {user_id:i for i,user_id in enumerate(list(set(df['userId'])))}
 movie_id_map = {movie_id:i for i,movie_id in enumerate(list(set(df['movieId'])))}
 
 df['userId'] = df['userId'].apply(lambda u: user_id_map[u])
 df['movieId'] = df['movieId'].apply(lambda m: movie_id_map[m])
 
-df.to_csv("ratings.csv", index=False)
+if os.path.isdir("data"):
+    shutil.rmtree("data")
+
+os.makedirs("data")
+
+df.to_csv("data/ratings.csv", index=False)
