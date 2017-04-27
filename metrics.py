327e04858c35168130b26ef86b4e3c2ac9539587
vvusirikala <vvusirikala@rmn.com>
2017-04-27 17:03:47 -0500

    Compute NDCG

--- metrics.py
+++ metrics.py
@@ -3,41 +3,46 @@ import pandas as pd
 import numpy as np
 
 # TODO: Ensure that groups retain order
-def precision_recall_at_N(results_df, test_df, top_N=6):
-    results_df = results_df.groupby('user').apply(lambda x: x.head(top_N))
+def precision_recall_at_N(recs_df, test_df, top_N=6):
+    recs_df = recs_df.groupby('user').apply(lambda x: x.head(top_N))
     test_df = test_df.groupby('user').apply(lambda x: x.head(top_N))
-    overlap_df = results_df.merge(test_df, how='inner', on=['user', 'movie'])
+    overlap_df = recs_df.merge(test_df, how='inner', on=['user', 'movie'])
 
     # TODO: save lengths instead to save memory
-    precision = len(overlap_df) / len(results_df)
+    precision = len(overlap_df) / len(recs_df)
     recall = len(overlap_df) / len(test_df)
     return precision, recall
 
 
-def precision_recall_threshold(results_df, test_df, thresh=3.0):
-    results_df = results_df[results_df['predicted_rating'] >= thresh]
+def precision_recall_threshold(recs_df, test_df, thresh=3.0):
+    recs_df = recs_df[recs_df['predicted_rating'] >= thresh]
     test_df = test_df[test_df['actual_rating'] >= thresh]
-    overlap_df = results_df.merge(test_df, how='inner', on=['user', 'movie'])
+    overlap_df = recs_df.merge(test_df, how='inner', on=['user', 'movie'])
 
     # TODO: save lengths instead to save memory
-    precision = len(overlap_df) / len(results_df)
+    precision = len(overlap_df) / len(recs_df)
     recall = len(overlap_df) / len(test_df)
     return precision, recall
 
 
-def ndcg(results_df, test_df, thresh=3.0):
+def compute_ndcg(recs_df, test_df, thresh=3.0):
     def rank(df):
         df['rank'] = range(1,len(df)+1)
 
-    results_df = results_df[results_df['predicted_rating'] >= thresh]
-    results_df = results_df.groupby('user').apply(rank)
+    recs_df = recs_df[recs_df['predicted_rating'] >= thresh]
+    recs_df = recs_df.groupby('user').apply(rank)
 
     test_df = test_df[test_df['actual_rating'] >= thresh]
     test_df = test_df.groupby('user').apply(rank)
-    overlap_df = results_df.merge(test_df, how='inner', on=['user', 'movie'], suffixes=('_predicted', '_actual'))
+    overlap_df = recs_df.merge(test_df, how='inner', on=['user', 'movie'], suffixes=('_predicted', '_actual'))
 
     dcg = overlap_df.groupby('user').apply(lambda x: np.sum(x['predicted_rating'] / np.log2(x['rank_predicted'] + 1)))
     idcg = overlap_df.groupby('user').apply(lambda x: np.sum(x['actual_rating'] / np.log2(x['rank_actual'] + 1)))
-    return np.mean(dcg / idcg)
+    ndcg = np.mean(dcg / idcg)
+    return ndcg
 
 
+def compute_rmse(recs_df, test_df):
+    overlap_df = recs_df.merge(test_df, how='inner', on=['user', 'movie'])
+    rmse = np.sqrt(np.mean((overlap_df['predicted_rating'] - overlap_df['actual_rating']) ** 2))
+    return rmse
