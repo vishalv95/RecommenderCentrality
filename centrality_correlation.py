baad4a0df3d6e3f9be155efafdddd84726c69a80
Caelan Evans <evans.caelan@gmail.com>
2017-04-29 17:32:29 -0500

    Correlation

--- centrality_correlation.py
+++ centrality_correlation.py
@@ -3,3 +3,8 @@ import numpy
 
 centrality_df = pd.read_csv("centrality_data/user_centrality.csv")
 print(centrality_df.corr())
+
+print("\n\n")
+
+centrality_df = pd.read_csv("centrality_data/movie_centrality.csv")
+print(centrality_df.corr())
