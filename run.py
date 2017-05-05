1b25dc78de1a0839ee4c60a09712ce293d31acb0
vvusirikala <vvusirikala@rmn.com>
2017-05-04 19:08:15 -0500

    Better csv naming for experiments

--- run.py
+++ run.py
@@ -16,5 +16,8 @@ method = sys.argv[1]
 centrality = sys.argv[2]
 alpha = float(sys.argv[3])
 print(cf.validation(*s.read_csv_data("data/ratings_med.csv"), method=method, centrality_measure=centrality, alpha=alpha))
-os.rename("recs.csv", "recs_{}_{}_{:3.1f}.csv".format(method, centrality, alpha))
+if alpha > 0:
+	os.rename("recs.csv", "recs_{}.csv".format(method))
+else:
+	os.rename("recs.csv", "recs_{}_{}_{:3.1f}.csv".format(method, centrality, alpha))
 #print(cf.validation(*s.read_csv_data("data/ratings_med.csv"), method="user_centrality", centrality_measure="betweenness", alpha=0))
